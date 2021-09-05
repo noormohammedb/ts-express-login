3d8ba45eab43e933031df9cd8d7f8b46ed3fda39
Noor Mohammed <mail@noormohammed.tech>
2021-09-05 16:02:43 +0530

    signup frontend
    signup route and form

--- routes/index.js
+++ routes/index.js
@@ -6,4 +6,16 @@ router.get("/", function (req, res, next) {
   res.render("index", { title: "Login" });
 });
 
+router.get("/login", function (req, res, next) {
+  res.render("index", { title: "Login" });
+});
+
+router.get("/signup", function (req, res) {
+  const hbsObj = {
+    title: "signup",
+    link: "login",
+  };
+  res.render("signup", hbsObj);
+});
+
 module.exports = router;
