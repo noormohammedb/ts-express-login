93a262213f2e0896c9daf4a671dfaf609fb163ff
Noor Mohammed <mail@noormohammed.tech>
2021-09-05 15:04:36 +0530

    database connection
    dot env for db credentials
    sample env file

--- app.js
+++ app.js
@@ -4,6 +4,11 @@ var path = require("path");
 var cookieParser = require("cookie-parser");
 var logger = require("morgan");
 
+const dbutil = require("./utils/db-connection");
+dbutil.dbconnect().then(() => {
+  console.log("db connected");
+});
+
 var indexRouter = require("./routes/index");
 var usersRouter = require("./routes/users");
 
---
+++ utils/db-connection.js
@@ -0,0 +1,46 @@
+require("dotenv").config();
+const { MongoClient, LEGAL_TCP_SOCKET_OPTIONS } = require("mongodb");
+
+const uri = process.env.DB_URI || "mongodb://127.0.0.1:27017";
+const database = process.env.DB_NAME || "login";
+const client = new MongoClient(uri, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+});
+
+async function dbConnection() {
+  try {
+    await client.connect();
+    return client;
+  } catch (error) {
+    console.log("connection error");
+    console.error(error);
+  }
+}
+
+// client.connect((err) => {
+//   const collection = client.db("test").collection("devices");
+//   // perform actions on the collection object
+//   console.log("db connected");
+//   db_insertin();
+//   // client.close();
+// });
+
+// async () => {
+const db_insertin = async () => {
+  try {
+    console.log("trying to db insertion");
+    await client.db("test").collection("test").insertOne({ key: "value" });
+    console.log("db insertion success");
+  } catch (error) {
+    console.log("db insertion error");
+
+    console.error(error);
+  }
+};
+
+module.exports = {
+  dbconnect: dbConnection,
+  db: client.db(database),
+  db_insertin,
+};
