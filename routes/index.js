5d3f4a56f6028b6d078054e30605ea821351ae75
Noor Mohammed <mail@noormohammed.tech>
2021-09-05 16:29:33 +0530

    signup api
    checking username exist in db
    and inserting new user

--- routes/index.js
+++ routes/index.js
@@ -1,5 +1,6 @@
 var express = require("express");
 var router = express.Router();
+const db = require("../utils/db-connection").db;
 
 /* GET home page. */
 router.get("/", function (req, res, next) {
@@ -16,6 +17,37 @@ router.get("/signup", function (req, res) {
     link: "login",
   };
   res.render("signup", hbsObj);
+});
+
+router.post("/login-submission", (req, res) => {
+  console.log("login submission");
+  console.log(req.body);
+  res.json({ message: "login-submission" });
+});
+
+router.post("/signup-submission", async (req, res) => {
+  console.log("signup submission");
+  console.log(req.body);
+  try {
+    const user = await db
+      .collection("user")
+      .findOne({ user_name: req.body.user_name });
+    console.info("user:", user);
+    if (user) {
+      res.json({ message: "signup user exist" });
+      console.log("user exist in db");
+    } else {
+      await db.collection("user").insertOne(req.body);
+      console.log("db insertion done");
+      res.json({ message: "signup success" });
+    }
+  } catch (error) {
+    console.log("error in signup db operation");
+    console.error(error);
+    res.status(500).json({ status: "internal server error" });
+  }
+
+  // res.json({ message: "signup-submission" });
 });
 
 module.exports = router;
